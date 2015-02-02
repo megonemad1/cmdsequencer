be452c1672a52dda6d75b8dc28e2eb7675478dd4
megonemad1 <rhys.puddephatt@btinternet.com>
2015-02-02 16:41:17 +0000

    create comand

---
+++ CmdSequence/AutoCmd/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCmd
+{
+    public class Command
+    {
+        string _command;
+        HashSet<argument> Param;
+        public Command(string Command)
+        {
+            this._command = Command;
+            Param = new HashSet<argument>();
+        }
+        public Command AddPeram(string PeramiterType, string arg)
+        {
+            Param.Add(new argument { Arg = arg, ArgType = PeramiterType });
+            return this;
+        }
+        public string GetCommand()
+        {
+            return _command + " " + String.Join(" ", Param);
+        }
+    }
+    struct argument
+    {
+        public string ArgType;
+        public string Arg;
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", ArgType, Arg);
+        }
+    }
+}
--- CmdSequence/CmdSequence/Program.cs
+++ CmdSequence/CmdSequence/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoCmd;
 
 namespace CmdSequence
 {
@@ -10,6 +11,12 @@ namespace CmdSequence
     {
         static void Main(string[] args)
         {
+            Command a = new Command("runthis");
+            a.AddPeram("-a", "structured");
+            a.AddPeram("-a", "structured2");
+            a.AddPeram("-d", "dg");
+            Console.WriteLine(a.GetCommand());
+            Console.ReadLine();
         }
     }
 }
